91b0bda819abd244c88be58b14eb39ec2bf76d3e
20091A0531 <130962155+20091A0531@users.noreply.github.com>
2023-04-19 13:19:55 +0530

    Update Date-19.py

--- Date-19.py
+++ Date-19.py
@@ -38,3 +38,71 @@ if(num==amg):
   print("It is a Armstrong number")
 else:
   print("It is not a Armstrong number")
+---------------------------
+area of triangle,square,rectangle
+
+i=1
+while(i<=3):
+  if(i==1):
+    h=int(input("Enter the height for the area of triangle:"))
+    b=int(input("Enter the breadth of the triangle:"))
+    print("area of triangle is {}".format(0.5*h*b))
+  elif(i==2):
+    s=int(input("Enter the side of the square:"))
+    print("area of square is {}".format(s*s))
+  else:
+    l=int(input("Enter the length for the area of rectangle:"))
+    b=int(input("Enter the breadth of the rectangle:"))
+    print("area of rectangle is {}".format(l*b))
+  i+=1
+----------------------------
+swaping three numbers in ascending order
+
+a=int(input("Enter the number1"))
+b=int(input("Enter the number2"))
+c=int(input("Enter the number3"))
+l3=0
+l2=0
+l1=0
+if(a>c and a>b):
+  l3=a
+  if(b<c):
+    l1=b
+    l2=c
+  else:
+    l1=c
+    l2=b
+elif(b>c and b>a):
+  l3=b
+  if(a<c):
+    l1=a
+    l2=c
+  else:
+    l1=c
+    l2=a
+else:
+  l3=c
+  if(b<a):
+    l1=b
+    l2=a
+  else:
+    l1=a
+    l2=b
+a,b,c=l1,l2,l3
+print(a,b,c)
+
+-----------------------------
+fibanocci series
+
+
+n=int(input("Enter the number,upto where you want the fibanocci numbers:"))
+a=0
+b=1
+print(a,b,end=" ")
+while(a+b<n):
+    c=a+b
+    print(c,end=" ")
+    a=b
+    b=c
+
+-----------------------
