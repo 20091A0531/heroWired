586b9e6d28df62ffd642f04f4966e5d78b6104ec
20091A0531 <130962155+20091A0531@users.noreply.github.com>
2023-04-19 11:28:46 +0530

    Update Date-19.py

--- Date-19.py
+++ Date-19.py
@@ -3,10 +3,23 @@
 333
 4444
 55555
---------
 n=int(input("Enter number of rows:"))
 for i in range(1,n+1):
   for j in range(i+1):
     print(i,end="")
   print()
+
+--------------------
+-->reverse of a number
+
+
+num=int(input("enter a number"))
+temp=num
+rev=0
+while(temp!=0):
+  rem=temp%10
+  rev=rev*10+rem
+  temp=temp/10
+print(rev)
+ 
 
